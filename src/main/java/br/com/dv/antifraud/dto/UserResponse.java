6dcf6e05e49f497cb62835e293c8198d9a5a156a
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-26 15:39:37 -0300

    3/6: include role in UserResponse and mapper

--- src/main/java/br/com/dv/antifraud/dto/UserResponse.java
+++ src/main/java/br/com/dv/antifraud/dto/UserResponse.java
@@ -1,4 +1,4 @@
 package br.com.dv.antifraud.dto;
 
-public record UserResponse(Long id, String name, String username) {
+public record UserResponse(Long id, String name, String username, String role) {
 }
---
+++ src/main/java/br/com/dv/antifraud/dto/UserStatusUpdateInfo.java
@@ -0,0 +1,13 @@
+package br.com.dv.antifraud.dto;
+
+import br.com.dv.antifraud.enums.UserOperation;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserStatusUpdateInfo(
+        @NotBlank(message = "Username is required.")
+        String username,
+        @NotNull(message = "Invalid operation. The available operations are LOCK and UNLOCK.")
+        UserOperation operation
+) {
+}
