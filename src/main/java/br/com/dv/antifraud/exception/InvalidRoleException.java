5da9999ed015552195c708a45a0092cbb4990937
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-26 15:50:51 -0300

    3/6: add PUT /api/auth/role endpoint
    - Created changeRole method in AuthController and AppUserService.
    
    - Created UserRoleUpdateInfo DTO.
    
    - Introduced custom exceptions InvalidRoleException and RoleAlreadyAssignedException for exception handling:
     * if a role is not SUPPORT or MERCHANT, respond with HTTP 400;
     * if the assigned role is already provided to the user, respond with HTTP 409;

---
+++ src/main/java/br/com/dv/antifraud/exception/InvalidRoleException.java
@@ -0,0 +1,9 @@
+package br.com.dv.antifraud.exception;
+
+public class InvalidRoleException extends RuntimeException {
+
+    public InvalidRoleException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/br/com/dv/antifraud/service/AppUserService.java
+++ src/main/java/br/com/dv/antifraud/service/AppUserService.java
@@ -3,6 +3,7 @@ package br.com.dv.antifraud.service;
 import br.com.dv.antifraud.dto.UserCreationInfo;
 import br.com.dv.antifraud.dto.UserResponse;
 import br.com.dv.antifraud.dto.UserDeletionResponse;
+import br.com.dv.antifraud.dto.UserRoleUpdateInfo;
 
 import java.util.List;
 
@@ -13,5 +14,7 @@ public interface AppUserService {
     List<UserResponse> findAll();
 
     UserDeletionResponse delete(String username);
+
+    UserResponse changeRole(UserRoleUpdateInfo roleUpdateInfo);
 
 }
