390e893cf89d8c349bbe8d48b161df9181df68e4
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-26 15:30:16 -0300

    3/6: enhance user registration logic
    - Updated register method in AppUserServiceImpl:
     1. The first registered user should receive the ADMINISTRATOR role; the rest should receive the MERCHANT role
     2. All users, except ADMINISTRATOR, must be locked immediately after registration
    
    - Implemented getOrCreateRole helper method.
    
    - Created RoleType enum.

--- src/main/java/br/com/dv/antifraud/service/AppUserServiceImpl.java
+++ src/main/java/br/com/dv/antifraud/service/AppUserServiceImpl.java
@@ -3,13 +3,17 @@ package br.com.dv.antifraud.service;
 import br.com.dv.antifraud.dto.UserCreationInfo;
 import br.com.dv.antifraud.dto.UserResponse;
 import br.com.dv.antifraud.dto.UserDeletionResponse;
+import br.com.dv.antifraud.entity.AppUserRole;
+import br.com.dv.antifraud.enums.RoleType;
 import br.com.dv.antifraud.exception.UserAlreadyExistsException;
 import br.com.dv.antifraud.mapper.AppUserMapper;
 import br.com.dv.antifraud.entity.AppUser;
 import br.com.dv.antifraud.repository.AppUserRepository;
+import br.com.dv.antifraud.repository.AppUserRoleRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,14 +22,21 @@ import java.util.Optional;
 public class AppUserServiceImpl implements AppUserService {
 
     private final AppUserRepository userRepository;
+    private final AppUserRoleRepository roleRepository;
     private final PasswordEncoder encoder;
 
-    public AppUserServiceImpl(AppUserRepository userRepository, PasswordEncoder encoder) {
+    public AppUserServiceImpl(
+            AppUserRepository userRepository,
+            AppUserRoleRepository roleRepository,
+            PasswordEncoder encoder
+    ) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.encoder = encoder;
     }
 
     @Override
+    @Transactional
     public UserResponse register(UserCreationInfo userCreationInfo) {
         Optional<AppUser> userOptional = userRepository.findByUsernameIgnoreCase(userCreationInfo.username());
 
@@ -34,7 +45,14 @@ public class AppUserServiceImpl implements AppUserService {
         }
 
         AppUser user = AppUserMapper.toEntity(userCreationInfo);
+
         user.setPassword(encoder.encode(user.getPassword()));
+
+        boolean isFirstUser = userRepository.count() == 0;
+        AppUserRole userRole = getOrCreateRole(isFirstUser ? RoleType.ADMINISTRATOR : RoleType.MERCHANT);
+        user.setRole(userRole);
+        user.setLocked(!isFirstUser);
+
         AppUser savedUser = userRepository.save(user);
 
         return AppUserMapper.toResponse(savedUser);
@@ -58,6 +76,14 @@ public class AppUserServiceImpl implements AppUserService {
         userRepository.delete(user);
 
         return AppUserMapper.toDeletionResponse(user);
+    }
+
+    private AppUserRole getOrCreateRole(RoleType role) {
+        return roleRepository.findByName(role.name()).orElseGet(() -> {
+            AppUserRole newRole = new AppUserRole();
+            newRole.setName(role.name());
+            return roleRepository.save(newRole);
+        });
     }
 
 }
