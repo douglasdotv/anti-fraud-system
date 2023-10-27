725e3483430f0e8cc0afd3732e31c522cae00ca1
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-27 16:36:40 -0300

    4/6: create StolenCardMapper

---
+++ src/main/java/br/com/dv/antifraud/mapper/StolenCardMapper.java
@@ -0,0 +1,34 @@
+package br.com.dv.antifraud.mapper;
+
+import br.com.dv.antifraud.dto.StolenCardDeletionResponse;
+import br.com.dv.antifraud.dto.StolenCardInfo;
+import br.com.dv.antifraud.dto.StolenCardResponse;
+import br.com.dv.antifraud.entity.StolenCard;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StolenCardMapper {
+
+    public static StolenCard dtoToEntity(StolenCardInfo cardInfo) {
+        StolenCard card = new StolenCard();
+        card.setCardNumber(cardInfo.number());
+        return card;
+    }
+
+    public static StolenCardResponse entityToDto(StolenCard card) {
+        return new StolenCardResponse(card.getId(), card.getCardNumber());
+    }
+
+    public static List<StolenCardResponse> entityToDtoList(List<StolenCard> stolenCards) {
+        return stolenCards.stream()
+                .map(StolenCardMapper::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    public static StolenCardDeletionResponse entityToDeletionDto(StolenCard card) {
+        String deletionStatus = String.format("Card %s successfully removed!", card.getCardNumber());
+        return new StolenCardDeletionResponse(deletionStatus);
+    }
+
+}
