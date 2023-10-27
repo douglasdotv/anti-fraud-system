5d84dae6e485ed6a7c712cd652118d7911123930
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-27 16:13:28 -0300

    4/6: create SuspiciousIpAddressMapper

---
+++ src/main/java/br/com/dv/antifraud/mapper/SuspiciousIpAddressMapper.java
@@ -0,0 +1,34 @@
+package br.com.dv.antifraud.mapper;
+
+import br.com.dv.antifraud.dto.SuspiciousIpDeletionResponse;
+import br.com.dv.antifraud.dto.SuspiciousIpInfo;
+import br.com.dv.antifraud.dto.SuspiciousIpResponse;
+import br.com.dv.antifraud.entity.SuspiciousIpAddress;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SuspiciousIpAddressMapper {
+
+    public static SuspiciousIpAddress dtoToEntity(SuspiciousIpInfo ipInfo) {
+        SuspiciousIpAddress ipAddress = new SuspiciousIpAddress();
+        ipAddress.setIpAddress(ipInfo.ip());
+        return ipAddress;
+    }
+
+    public static SuspiciousIpResponse entityToDto(SuspiciousIpAddress ipAddress) {
+        return new SuspiciousIpResponse(ipAddress.getId(), ipAddress.getIpAddress());
+    }
+
+    public static List<SuspiciousIpResponse> entityToDtoList(List<SuspiciousIpAddress> suspiciousIpAddresses) {
+        return suspiciousIpAddresses.stream()
+                .map(SuspiciousIpAddressMapper::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    public static SuspiciousIpDeletionResponse entityToDeletionDto(SuspiciousIpAddress ipAddress) {
+        String deletionStatus = String.format("IP %s successfully removed!", ipAddress.getIpAddress());
+        return new SuspiciousIpDeletionResponse(deletionStatus);
+    }
+
+}
