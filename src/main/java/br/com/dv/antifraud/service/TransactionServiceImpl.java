4916078a2f4e6a760e9fa048817b4af7102bcc4b
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-27 17:05:27 -0300

    4/6: implement new transaction rules
    - If an IP address is in the blacklist, the transaction is PROHIBITED.
    - If a card number is in the blacklist, the transaction is PROHIBITED.
    - If the result is ALLOWED, the info field must be set to none.
    - In the case of PROHIBITED or MANUAL_PROCESSING, the info field must contain the reason for rejecting the transaction; the reason must be separated by , and sorted alphabetically. For example, amount, card-number, ip.

--- src/main/java/br/com/dv/antifraud/service/TransactionServiceImpl.java
+++ src/main/java/br/com/dv/antifraud/service/TransactionServiceImpl.java
@@ -3,15 +3,65 @@ package br.com.dv.antifraud.service;
 import br.com.dv.antifraud.dto.TransactionInfo;
 import br.com.dv.antifraud.dto.TransactionResponse;
 import br.com.dv.antifraud.enums.TransactionResult;
+import br.com.dv.antifraud.repository.StolenCardRepository;
+import br.com.dv.antifraud.repository.SuspiciousIpAddressRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
+    private final SuspiciousIpAddressRepository suspiciousIpRepository;
+    private final StolenCardRepository stolenCardRepository;
+
+    public TransactionServiceImpl(SuspiciousIpAddressRepository suspiciousIpRepository,
+                                  StolenCardRepository stolenCardRepository) {
+        this.suspiciousIpRepository = suspiciousIpRepository;
+        this.stolenCardRepository = stolenCardRepository;
+    }
+
     @Override
+    @Transactional(readOnly = true)
     public TransactionResponse processTransaction(TransactionInfo transactionInfo) {
-        TransactionResult result = TransactionResult.fromAmount(transactionInfo.amount());
-        return new TransactionResponse(result.toString());
+        Long amount = transactionInfo.amount();
+        TransactionResult result = TransactionResult.fromAmount(amount);
+
+        boolean isStolenCard = stolenCardRepository.findByCardNumber(transactionInfo.number()).isPresent();
+        boolean isSuspiciousIp = suspiciousIpRepository.findByIpAddress(transactionInfo.ip()).isPresent();
+
+        if (isStolenCard || isSuspiciousIp) {
+            result = TransactionResult.PROHIBITED;
+        }
+
+        String info = getInfo(result, amount, isStolenCard, isSuspiciousIp);
+
+        return new TransactionResponse(result.name(), info);
+    }
+
+    private String getInfo(TransactionResult result, Long amount, boolean stolenCard, boolean suspiciousIp) {
+        switch (result) {
+            case ALLOWED -> {
+                return "none";
+            }
+            case MANUAL_PROCESSING -> {
+                return "amount";
+            }
+            case PROHIBITED -> {
+                List<String> reasons = new ArrayList<>();
+
+                if (amount > 1500) reasons.add("amount");
+                if (stolenCard) reasons.add("card-number");
+                if (suspiciousIp) reasons.add("ip");
+
+                return String.join(", ", reasons);
+            }
+            default -> {
+                return "";
+            }
+        }
     }
 
 }
