6865ef3fcaf13a0ea6264c6fc6c611b80f7a0297
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-27 17:28:29 -0300

    4/6: update authZ rules
    Included rules for suspicious-ip and stolencard endpoints.
    (And removed unnecessary code comments.)

--- src/main/java/br/com/dv/antifraud/security/SecurityConfig.java
+++ src/main/java/br/com/dv/antifraud/security/SecurityConfig.java
@@ -32,63 +32,54 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
-        /*
-         * Set Http Basic Auth
-         * Disable CSRF protection to facilitate testing (not intended for production)
-         * Disable frame options to allow H2 console (not intended for production)
-         * Handle authentication errors
-         * Define authorization rules:
-         *  - /api/auth/user POST: permit all
-         *  - /api/auth/user DELETE, /api/auth/role PUT and /api/auth/access PUT: only ADMINISTRATOR
-         *  - /api/auth/list GET: only ADMINISTRATOR and SUPPORT
-         *  - /api/antifraud/transaction POST: only MERCHANT
-         *  - /actuator/shutdown: permit all (for Hyperskill tests, not intended for production)
-         *  - h2: permit all (not intended for production)
-         *  - any other request: require authentication
-         * Set stateless Basic Auth: no session
-         * Set Spring Security to use UserDetailsService implementation
-         */
         return http
                 .httpBasic(Customizer.withDefaults())
-                .csrf(CsrfConfigurer::disable)
                 .exceptionHandling(cfg ->
                         cfg.authenticationEntryPoint(restAuthenticationEntryPoint)
-                )
-                .headers(cfg ->
-                        cfg.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
                 )
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/auth/user"))
                         .permitAll()
 
-                        .requestMatchers(mvc.pattern(HttpMethod.DELETE, "/api/auth/user/*"))
-                        .hasRole("ADMINISTRATOR")
-
-                        .requestMatchers(mvc.pattern(HttpMethod.PUT, "/api/auth/role/**"))
-                        .hasRole("ADMINISTRATOR")
-
-                        .requestMatchers(mvc.pattern(HttpMethod.PUT, "/api/auth/access/**"))
-                        .hasRole("ADMINISTRATOR")
+                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/antifraud/transaction/**"))
+                        .hasRole("MERCHANT")
 
                         .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/auth/list"))
                         .hasAnyRole("ADMINISTRATOR", "SUPPORT")
 
-                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/antifraud/transaction/**"))
-                        .hasRole("MERCHANT")
+                        .requestMatchers(
+                                mvc.pattern(HttpMethod.DELETE, "/api/auth/user/*"),
+                                mvc.pattern(HttpMethod.PUT, "/api/auth/role/**"),
+                                mvc.pattern(HttpMethod.PUT, "/api/auth/access/**")
+                        ).hasRole("ADMINISTRATOR")
 
-                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/actuator/shutdown"))
-                        .permitAll()
+                        .requestMatchers(
+                                mvc.pattern(HttpMethod.POST, "/api/antifraud/stolencard"),
+                                mvc.pattern(HttpMethod.DELETE, "/api/antifraud/stolencard/*"),
+                                mvc.pattern(HttpMethod.GET, "/api/antifraud/stolencard"),
+                                mvc.pattern(HttpMethod.POST, "/api/antifraud/suspicious-ip"),
+                                mvc.pattern(HttpMethod.DELETE, "/api/antifraud/suspicious-ip/*"),
+                                mvc.pattern(HttpMethod.GET, "/api/antifraud/suspicious-ip")
+                        ).hasRole("SUPPORT")
 
-                        .requestMatchers(PathRequest.toH2Console())
-                        .permitAll()
+                        // For Hyperskill automated tests (not intended for production)
+                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/actuator/shutdown")).permitAll()
 
-                        .anyRequest()
-                        .authenticated()
+                        // For H2 console (not intended for production)
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+
+                        .anyRequest().permitAll()
                 )
                 .sessionManagement(cfg ->
                         cfg.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .userDetailsService(userDetailsService)
+                // Disable CSRF protection to facilitate testing (not intended for production)
+                .csrf(CsrfConfigurer::disable)
+                // Disable frame options to allow H2 console (not intended for production)
+                .headers(cfg ->
+                        cfg.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
                 .build();
     }
 
