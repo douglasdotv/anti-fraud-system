fdee597dbd2bb30cb013ae79bc94b93e78e9d321
Douglas Vieira <douglasdotv@hotmail.com>
2023-10-27 16:13:10 -0300

    4/6: create DTOs for ip
    Created SuspiciousIpInfo, SuspiciousIpResponse and SuspiciousIpDeletionResponse DTOs.

---
+++ src/main/java/br/com/dv/antifraud/dto/SuspiciousIpInfo.java
@@ -0,0 +1,9 @@
+package br.com.dv.antifraud.dto;
+
+import jakarta.validation.constraints.Pattern;
+
+public record SuspiciousIpInfo(
+        @Pattern(regexp = "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)(\\.(?!$)|$)){4}$", message = "Invalid IPv4 format.")
+        String ip
+) {
+}
