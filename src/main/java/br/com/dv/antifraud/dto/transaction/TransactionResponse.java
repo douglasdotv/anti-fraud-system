1e66ae0f59001aff2ad39be77cfeb70a4c928ad5
Douglas Vieira <douglas16722@gmail.com>
2023-10-28 15:26:28 -0300

    5/6: update transaction DTOs
    Updated TransactionRequest to include date and region (and validations).
    Updated TransactionResponse result field type from String to TransactionResult.

--- src/main/java/br/com/dv/antifraud/dto/transaction/TransactionResponse.java
+++ src/main/java/br/com/dv/antifraud/dto/transaction/TransactionResponse.java
@@ -1,4 +1,6 @@
 package br.com.dv.antifraud.dto.transaction;
 
-public record TransactionResponse(String result, String info) {
+import br.com.dv.antifraud.enums.TransactionResult;
+
+public record TransactionResponse(TransactionResult result, String info) {
 }
