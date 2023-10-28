daa8d31986f50317d18bf7a2b730e49d24618d23
Douglas Vieira <douglas16722@gmail.com>
2023-10-28 15:26:28 -0300

    5/6: update transaction DTOs
    Updated TransactionRequest to include date and region (and validations).
    Updated TransactionResponse result field type from String to TransactionResult.

--- src/main/java/br/com/dv/antifraud/dto/transaction/TransactionRequest.java
+++ src/main/java/br/com/dv/antifraud/dto/transaction/TransactionRequest.java
@@ -1,9 +1,13 @@
 package br.com.dv.antifraud.dto.transaction;
 
+import br.com.dv.antifraud.enums.WorldRegion;
 import br.com.dv.antifraud.validation.CardNumber;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDateTime;
 
 public record TransactionRequest(
         @NotNull(message = "Amount is required.")
@@ -12,6 +16,10 @@ public record TransactionRequest(
         @Pattern(regexp = "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)(\\.(?!$)|$)){4}$", message = "Invalid IPv4 format.")
         String ip,
         @CardNumber
-        String number
+        String number,
+        @NotNull(message = "Invalid region. The available regions are: EAP, ECA, HIC, LAC, MENA, SA and SSA.")
+        WorldRegion region,
+        @PastOrPresent(message = "Date must be in the past or present.")
+        LocalDateTime date
 ) {
 }
