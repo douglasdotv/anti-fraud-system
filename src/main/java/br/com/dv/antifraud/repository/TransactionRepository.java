16dc69c0ed472526c3230fa7ac5de93ea22ca426
Douglas Vieira <douglas16722@gmail.com>
2023-10-28 15:42:39 -0300

    5/6: add region and IP count queries
    - Extended TransactionRepository: added queries to count distinct IPs/regions in the last hour

--- src/main/java/br/com/dv/antifraud/repository/TransactionRepository.java
+++ src/main/java/br/com/dv/antifraud/repository/TransactionRepository.java
@@ -1,7 +1,32 @@
 package br.com.dv.antifraud.repository;
 
 import br.com.dv.antifraud.entity.Transaction;
+import br.com.dv.antifraud.enums.WorldRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("""
+            SELECT COUNT(DISTINCT t.region)
+            FROM Transaction t
+            WHERE t.cardNumber = :cardNumber
+            AND t.region != :region
+            AND t.date BETWEEN :oneHourBefore AND :now
+            """)
+    Long countDistinctRegionsInLastHour(String cardNumber, WorldRegion region,
+                                        LocalDateTime oneHourBefore, LocalDateTime now);
+
+    @Query("""
+            SELECT COUNT(DISTINCT t.ip)
+            FROM Transaction t
+            WHERE t.cardNumber = :cardNumber
+            AND t.ip != :ip
+            AND t.date BETWEEN :oneHourBefore AND :now
+            """)
+    Long countDistinctIpsInLastHour(String cardNumber, String ip,
+                                    LocalDateTime oneHourBefore, LocalDateTime now);
+
 }
