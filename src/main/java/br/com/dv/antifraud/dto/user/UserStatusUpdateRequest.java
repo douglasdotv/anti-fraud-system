467cd1189abc58c414bb2d1adbb45b0d027ec6f5
Douglas Vieira <douglas16722@gmail.com>
2023-10-29 10:34:54 -0300

    fix: use custom annotation to validate enum values
    Create @EnumValue custom annotation and use it to properly validate enum values in DTOs.

--- src/main/java/br/com/dv/antifraud/dto/user/UserStatusUpdateRequest.java
+++ src/main/java/br/com/dv/antifraud/dto/user/UserStatusUpdateRequest.java
@@ -1,13 +1,15 @@
 package br.com.dv.antifraud.dto.user;
 
 import br.com.dv.antifraud.enums.UserOperation;
+import br.com.dv.antifraud.validation.EnumValue;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record UserStatusUpdateRequest(
         @NotBlank(message = "Username is required.")
         String username,
-        @NotNull(message = "Invalid operation. The available operations are LOCK and UNLOCK.")
+        @EnumValue(
+                enumClass = UserOperation.class,
+                message = "Invalid operation. The available operations are LOCK and UNLOCK.")
         UserOperation operation
 ) {
 }
