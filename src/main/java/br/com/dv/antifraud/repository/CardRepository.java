041a1a344512e1e00a6dfc359327be7281cfacba
Douglas Vieira <douglas16722@gmail.com>
2023-11-01 00:15:27 -0300

    6/6: add findByCardNumber to CardRepository

--- src/main/java/br/com/dv/antifraud/repository/CardRepository.java
+++ src/main/java/br/com/dv/antifraud/repository/CardRepository.java
@@ -3,5 +3,10 @@ package br.com.dv.antifraud.repository;
 import br.com.dv.antifraud.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    Optional<Card> findByCardNumber(String cardNumber);
+
 }
