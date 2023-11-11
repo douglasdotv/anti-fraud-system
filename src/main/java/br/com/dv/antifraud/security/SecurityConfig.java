dea2ae31d9bafe3e18eeba2c523747ad34010945
Douglas Vieira <douglas16722@gmail.com>
2023-11-10 22:31:22 -0300

    refactor: simplify security config
    - Removed @EnableWebSecurity and UserDetailsService from SecurityConfig, because Spring Boot autoconfigures it.
    - Replaced RestAuthenticationEntryPoint dependency injection with direct instantiation (and removed @Component annotation from RestAuthenticationEntryPoint).
    - Used @PreAuthorized in some controllers method to clean SecurityConfig class authorization rules.
    - Removed comments from SecurityConfig...

--- src/main/java/br/com/dv/antifraud/security/SecurityConfig.java
+++ src/main/java/br/com/dv/antifraud/security/SecurityConfig.java
@@ -5,12 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,73 +17,25 @@ import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
-
-    private final UserDetailsService userDetailsService;
-    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-
-    public SecurityConfig(UserDetailsService userDetailsService,
-                          RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
-        this.userDetailsService = userDetailsService;
-        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         return http
                 .httpBasic(Customizer.withDefaults())
-                .exceptionHandling(cfg ->
-                        cfg.authenticationEntryPoint(restAuthenticationEntryPoint)
-                )
+                .exceptionHandling(cfg -> cfg.authenticationEntryPoint(new RestAuthenticationEntryPoint()))
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/auth/user"))
-                        .permitAll()
-
-                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/antifraud/transaction/**"))
-                        .hasRole("MERCHANT")
-
-                        .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/auth/list"))
-                        .hasAnyRole("ADMINISTRATOR", "SUPPORT")
-
-                        .requestMatchers(
-                                mvc.pattern(HttpMethod.DELETE, "/api/auth/user/*"),
-                                mvc.pattern(HttpMethod.PUT, "/api/auth/role/**"),
-                                mvc.pattern(HttpMethod.PUT, "/api/auth/access/**")
-                        ).hasRole("ADMINISTRATOR")
-
-                        .requestMatchers(
-                                mvc.pattern(HttpMethod.POST, "/api/antifraud/stolencard"),
-                                mvc.pattern(HttpMethod.DELETE, "/api/antifraud/stolencard/*"),
-                                mvc.pattern(HttpMethod.GET, "/api/antifraud/stolencard"),
-                                mvc.pattern(HttpMethod.POST, "/api/antifraud/suspicious-ip"),
-                                mvc.pattern(HttpMethod.DELETE, "/api/antifraud/suspicious-ip/*"),
-                                mvc.pattern(HttpMethod.GET, "/api/antifraud/suspicious-ip"),
-                                mvc.pattern(HttpMethod.PUT, "/api/antifraud/transaction"),
-                                mvc.pattern(HttpMethod.GET, "/api/antifraud/history/**")
-                        ).hasRole("SUPPORT")
-
-                        // For tests
-                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/actuator/shutdown"))
-                        .permitAll()
-
-                        // For H2
-                        .requestMatchers(PathRequest.toH2Console())
-                        .permitAll()
-
-                        .anyRequest()
-                        .authenticated()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/actuator/shutdown")).permitAll()
+                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/auth/user")).permitAll()
+                        .requestMatchers(mvc.pattern("/api/antifraud/suspicious-ip/**")).hasRole("SUPPORT")
+                        .requestMatchers(mvc.pattern("/api/antifraud/stolencard/**")).hasRole("SUPPORT")
+                        .anyRequest().authenticated()
                 )
-                .sessionManagement(cfg ->
-                        cfg.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .userDetailsService(userDetailsService)
-                // Disable CSRF protection to facilitate testing
+                .sessionManagement(cfg -> cfg.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(CsrfConfigurer::disable)
-                // Disable frame options to allow H2
-                .headers(cfg ->
-                        cfg.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
-                )
+                .headers(cfg -> cfg.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .build();
     }
 
